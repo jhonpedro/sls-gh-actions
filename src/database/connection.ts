54b1a6e747c9c57442a99af547bafd6c5c6c1022
João Pedro Barros Ferreira <pedrobarros2010@gmail.com>
2021-11-07 18:59:44 -0300

    perf: add sequelize config options

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,4 +3,12 @@ import sequelizeConfig from '../../sequelizeConfig.js'
 
 export const sequelize = new Sequelize({
 	...sequelizeConfig,
+	define: { underscored: true, omitNull: false, raw: true },
+	pool: {
+		max: 2,
+		min: 0,
+	},
+	dialectOptions: {
+		connectTimeout: 60000,
+	},
 })
